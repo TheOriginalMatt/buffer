e34e12319e717adc312ece4a6a224627d7076506
Matt J <mdjackson97@gmail.com>
2024-01-21 14:06:50 -0500

    Added HandleCss.js, which allows us to programamatically add and remove CSS files, depending on which throbber has been selected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// JS files for individual Throbbers
 import { Classic } from './throbbers/classic/script.js';
 
 let myClassic = new Classic();
--- throbbers/classic/script.js
+++ throbbers/classic/script.js
@@ -1,6 +1,22 @@
+import { HandleCss } from "../../lib/HandleCss.js";
+
 export class Classic {
+	constructor() {
+		this.cssFilePath = "./throbbers/classic/style.css";
+		this.state = 0; // Waiting to be initialized
+		this.cssHandler = null;
+	}
+
 	init() {
 		console.debug("Classic - init()");
+		if (this.state != 0) {
+			console.error("You cannot call init() if the throbber is already running.");
+			return;
+		}
+		this.cssHandler = new HandleCss(this.cssFilePath);
+		this.cssHandler.addCss();
+
+		this.state = 1; // Initialized, waiting to run.
 	}
 
 	run() {
@@ -9,5 +25,6 @@ export class Classic {
 
 	halt() {
 		console.debug("Classic - halt()");
+		this.cssHandler.removeCss();
 	}
 };
